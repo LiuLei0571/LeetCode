dbe54672dd5cb4e58385ac03e0e3e8f8f0825c54
liulei <liulei@getui.com>
2018-12-06 23:17:26 +0800

    no message

--- src/Solution100.java
+++ src/Solution100.java
@@ -2,4 +2,15 @@
  * Created by liulei on 2018/12/5.
  */
 public class Solution100 {
+    public static void main(String[] args) {
+        int[] a = {2, 1, 3};
+        int[] b = {2, 1, 4};
+        TreeNode t1 = new TreeNode(a);
+        TreeNode t2 = new TreeNode(b);
+        System.out.println(new Solution100().isSameTree(t1.root, t2.root));
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return p == null && q == null || !(p == null || q == null) && p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
 }
--- src/Solution145.java
+++ src/Solution145.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by liulei on 2018/12/5.
  */
 public class Solution145 {
+    public static void main(String[] args) {
+
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> mList = new ArrayList<>();
+        postorderTraversal(root, mList);
+        return mList;
+    }
+
+    public void postorderTraversal(TreeNode root, List<Integer> mList) {
+        if (root == null) {
+            return ;
+        }
+        postorderTraversal(root.left, mList);
+        postorderTraversal(root.right, mList);
+        mList.add(root.val);
+    }
 }
