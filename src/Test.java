ae825fe65b486acb1b22349fe805a1c4ba689206
liulei <liulei@getui.com>
2019-05-20 14:39:07 +0800

    代码更新

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+public class Test {
+    private static int FLAG_CHECK_XM;
+    private static int FLAG_CHECK_VIVO;
+    public static String useMultiPuh="huawe,i,xiaomi,oppo,vivo,honor,meizu";
+
+    public static void main(String[] args) {
+        Integer a=1;
+        Integer b=1;
+        Integer c=200;
+        Integer d=200;
+        System.out.println(a==b);
+        System.out.println(c==d);
+    }
+}
