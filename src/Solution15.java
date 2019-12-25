d1861806fc9dd57b5f81e1681a71e723917fbdc6
liulei <liulei@getui.com>
2019-12-25 17:50:40 +0800

    solution 15 三数之和

--- src/Solution15.java
+++ src/Solution15.java
@@ -1,31 +1,56 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Solution15 {
     public static void main(String[] args) {
-        int[] s=new int[]{-2,1,1,5,-1,0};
+        int[] s = new int[]{-2, 1, 1, 5, -1, 0,1};
         System.out.println(new Solution15().threeSum(s));
     }
+
+    //    public List<List<Integer>> threeSum(int[] nums) {
+//        List<List<Integer>> result=new ArrayList<>();
+//        int n=nums.length;
+//        for (int i = 0; i <n ; i++) {
+//            for (int j = i+1; j <n ; j++) {
+//                for (int k = j+1; k <n ; k++) {
+//                    if (nums[i]+nums[k]+nums[j]==0){
+//                        List<Integer> temp=new ArrayList<>();
+//                        temp.add(nums[i]);
+//                        temp.add(nums[j]);
+//                        temp.add(nums[k]);
+//                        result.add(temp);
+//                    }
+//                }
+//            }
+//        }
+//        HashSet set = new HashSet(result);
+//        result.clear();
+//        result.addAll(set);
+//        return result;
+//    }
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result=new ArrayList<>();
-        int n=nums.length;
-        for (int i = 0; i <n ; i++) {
-            for (int j = i+1; j <n ; j++) {
-                for (int k = j+1; k <n ; k++) {
-                    if (nums[i]+nums[k]+nums[j]==0){
-                        List<Integer> temp=new ArrayList<>();
-                        temp.add(nums[i]);
-                        temp.add(nums[j]);
-                        temp.add(nums[k]);
-                        result.add(temp);
-                    }
+        if (nums == null || nums.length < 3) {
+            return new ArrayList<>();
+        }
+
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (nums[0]==0&&nums[nums.length-1]==0){
+            result.add(Arrays.asList(0,0,0));
+            return result;
+        }
+        HashMap<Integer, Integer> hashMAp = new HashMap<>();
+        for (int i = 0; i <nums.length ; i++) {
+            for (int j = i+1; j <nums.length ; j++) {
+                if (hashMAp.containsKey(0-nums[i]-nums[j])){
+                    result.add(Arrays.asList(nums[i],nums[j],0-nums[i]-nums[j]));
                 }
             }
+            hashMAp.put(nums[i],i);
         }
-        HashSet set = new HashSet(result);
+        HashSet hashSet=new HashSet(result);
         result.clear();
-        result.addAll(set);
+        result.addAll(hashSet);
         return result;
     }
 }
