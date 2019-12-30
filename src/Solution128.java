867c75f0bc0c40d4581e5ba556f73db8ea897134
liulei <liulei@getui.com>
2019-12-30 19:35:13 +0800

    solution 128 最长连续序列

--- src/Solution128.java
+++ src/Solution128.java
@@ -1,6 +1,34 @@
-public class Solution128 {
-    public int longestConsecutive(int[] nums) {
+import java.util.HashMap;
 
-        return 0;
+public class Solution128 {
+    public static void main(String[] args) {
+        int[] a=new int[]{100,2,1,3,4};
+        System.out.println(new Solution128().longestConsecutive(a));
+    }
+
+    public int longestConsecutive(int[] nums) {
+        if (nums == null||nums.length==0) {
+            return 0;
+        }
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.getOrDefault(nums[i], 0) == 0) {
+                //相邻左边数字的最大序列
+                int left = hashMap.getOrDefault(nums[i] - 1, 0);
+                //相邻右边数字的最大序列
+                int right = hashMap.getOrDefault(nums[i] + 1, 0);
+                int length = left + right + 1;
+                hashMap.put(nums[i], length);
+                if (left != 0) {
+                    hashMap.put(nums[i] - left, length);
+                }
+                if (right != 0) {
+                    hashMap.put(nums[i] + right, length);
+                }
+                max = Math.max(max, length);
+            }
+        }
+        return max;
     }
 }
